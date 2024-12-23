98403fa5ac952659e279727104dffb9d59efdb35
j9nos <olah.janos.gergely@gmail.com>
2024-12-23 11:11:14 +0100

    slight fix

--- index_select.sh
+++ index_select.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
-# author:j9nos
 
 function select_by_index() {
     : '
-        1. output reference
-        2. read message
-        3. mapfile
+        1. Output reference
+        2. Read message
+        3. Mapfile
     '
-    local outref=$1; shift
-    local readonly message=$1; shift
-    local readonly options=("$@")
+    declare -r outref="$1"; shift
+    declare -r message="$1"; shift
+    declare -r options=("$@")
+    
     for i in "${!options[@]}"; do
         echo "[$i] --> ${options[$i]}"
     done
+    
     local selected=""
     while [[ -z "$selected" ]]; do
-        read -p "$message" index
+        read -r -p "$message" index
+        
         if [[ "$index" =~ ^[0-9]+$ ]] && [ "$index" -ge 0 ] && [ "$index" -lt ${#options[@]} ]; then
             selected="${options[$index]}"
+        else
+            echo "Your options are between 0 and $(( ${#options[@]} - 1 ))."
         fi
     done
-    eval $outref='$selected'
+    printf -v "$outref" '%s' "$selected"
 }
 
-function select_action() {
-    local actions
-    mapfile -t actions < <(echo -e "GO UP\nGO DOWN\nGO RIGHT\nGO LEFT")
-    select_by_index action "Select an action by index: " "${actions[@]}"
-    echo "You selected: $action"
-}
-
-
-select_action
+mapfile -t actions < <(echo -e "GO UP\nGO DOWN\nGO RIGHT\nGO LEFT")
+select_by_index action "Select an action by index: " "${actions[@]}"
+echo "$action"
